391e822e39549a4f659465c81c2a3dd57eb2f360
Robert Kirchner <rjojjr91@live.com>
2024-07-31 17:06:32 -0500

    refactor TunerException types - rk

--- src/main/exception/exceptions.py
+++ src/main/exception/exceptions.py
@@ -2,43 +2,48 @@ from typing import Callable
 
 
 class TunerException(Exception):
-    def __init__(self, message: str):
+    def __init__(self, message: str, exception_type: str = "GENERIC", sub_type: str | None = None):
         super(TunerException, self).__init__(message)
+        self.exception_type = exception_type
+        self.sub_type = sub_type
 
 
 class ValidationException(TunerException):
-    def __init__(self, message: str):
-        super(ValidationException, self).__init__(message)
-        self.type = 'VALIDATION'
+    def __init__(self, message: str, sub_type: str):
+        super(ValidationException, self).__init__(message, 'VALIDATION', sub_type)
 
 
 class ArgumentValidationException(ValidationException):
     def __init__(self, message: str):
-        super(ArgumentValidationException, self).__init__(message)
-        self.sub_type = 'ARGUMENT_VALIDATION'
+        super(ArgumentValidationException, self).__init__(message, 'ARGUMENT_VALIDATION')
 
 
 class HuggingfaceException(TunerException):
-    def __init__(self, message: str):
-        super(HuggingfaceException, self).__init__(message)
-        self.type = 'HUGGINGFACE'
+    def __init__(self, message: str, sub_type: str):
+        super(HuggingfaceException, self).__init__(message, 'HUGGINGFACE', sub_type)
 
 
 class HuggingfaceAuthException(HuggingfaceException):
     def __init__(self, message: str):
-        super(HuggingfaceAuthException, self).__init__(message)
-        self.sub_type = 'HUGGINGFACE_AUTH'
+        super(HuggingfaceAuthException, self).__init__(message, 'HUGGINGFACE_AUTH')
 
 
 def exception_handler(work: Callable, title: str) -> None:
     try:
         work()
     except TunerException as e:
-        print(f"A TunerException has happened: {str(e)}")
+        print('')
+        print(f"A TunerException has been caught: {str(e)}")
+        if e.exception_type == 'VALIDATION' and e.sub_type == 'ARGUMENT_VALIDATION':
+            print('')
+            print("Please verify that your arguments are valid")
+        print('')
         print(f"{title} is being terminated!")
         exit(1)
     except Exception as e:
+        print('')
         print(f"An unexpected Exception has been caught: {str(e)}")
+        print('')
         print(f"Rethrowing exception")
         raise e
 
