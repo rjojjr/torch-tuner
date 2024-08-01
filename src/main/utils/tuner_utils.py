1c3a21bf5b3027e11bcfd36cee3c209b0a2be64f
Robert Kirchner <rjojjr91@live.com>
2024-08-01 12:46:59 -0500

    refactor supported llm type check - rk

--- src/main/utils/tuner_utils.py
+++ src/main/utils/tuner_utils.py
@@ -6,7 +6,17 @@ from main.exception.exceptions import ArgumentValidationException
 
 def get_tuner(prog_args) -> Tuner:
     tuner = Tuner(llama.fine_tune, llama.merge, llama.push, LLM_TYPES['llama'])
-    if prog_args.llm_type != 'llama':
-        raise ArgumentValidationException(f'LLM type "{prog_args.llm_type}" not supported yet, ONLY "llama" is supported currently')
+    _evaluate_supported_llm_type(prog_args.llm_type)
 
     return tuner
+
+
+def _evaluate_supported_llm_type(llm_type):
+    is_supported = False
+    for key in LLM_TYPES:
+        if llm_type == key:
+            is_supported = True
+            break
+
+    if not is_supported:
+        raise ArgumentValidationException(f'LLM type "{llm_type}" not supported yet, ONLY "llama" is supported currently')
