aa68663629afdc1fd1a1580d399688711be7bc7e
Robert Kirchner <rjojjr91@live.com>
2024-08-01 12:38:06 -0500

    fix missing LLM type in git - rk

--- src/main/tuner.py
+++ src/main/tuner.py
@@ -6,7 +6,8 @@ LLM_TYPES = {'llama': 'LLAMA'}
 
 
 class Tuner:
-    def __init__(self, fine_tune: Callable[[arguments.TuneArguments], None], merge: Callable[[arguments.MergeArguments], None], push: Callable[[arguments.PushArguments], None]):
+    def __init__(self, fine_tune: Callable[[arguments.TuneArguments], None], merge: Callable[[arguments.MergeArguments], None], push: Callable[[arguments.PushArguments], None], llm_type: str = LLM_TYPES['llama']):
         self.fine_tune = fine_tune
         self.merge = merge
         self.push = push
+        self.llm_type = llm_type
