847f8c47621d9db002deed6817c960bce7650c4c
Robert Kirchner <rjojjr91@live.com>
2024-08-10 14:37:48 -0500

    fix get modules - rk

--- src/main/utils/model_utils.py
+++ src/main/utils/model_utils.py
@@ -7,10 +7,11 @@ def get_all_layers(model):
 
     for name, module in model.named_modules():
         if isinstance(module, (torch.nn.Linear, torch.nn.Embedding, torch.nn.Conv2d, Conv1D)):
+            module_name = '.'.join(name.split('.')[4:]).split('.')[0]
+            if module_name.strip() != '':
+                layers.append(module_name)
 
-            layers.append('.'.join(name.split('.')[4:]).split('.')[0])
-
-    return layers
+    return list(set(layers))
 
 
 def get_all_linear_layers(model):
@@ -18,7 +19,8 @@ def get_all_linear_layers(model):
 
     for name, module in model.named_modules():
         if isinstance(module, torch.nn.Linear):
+            module_name = '.'.join(name.split('.')[4:]).split('.')[0]
+            if module_name.strip() != '':
+                layers.append(module_name)
 
-            layers.append('.'.join(name.split('.')[4:]).split('.')[0])
-
-    return layers
+    return list(set(layers))
