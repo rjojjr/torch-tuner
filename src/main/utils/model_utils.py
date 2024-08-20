257ee8621c62373183d5b0403d0bd60c06f5cc7a
Robert Kirchner <rjojjr91@live.com>
2024-08-20 18:23:42 -0500

    add function descriptions && refactor model layers functions to be less repetitive - rk

--- src/main/utils/model_utils.py
+++ src/main/utils/model_utils.py
@@ -1,24 +1,23 @@
 import torch
 from transformers import Conv1D
 
+all_modules = (torch.nn.Linear, torch.nn.Embedding, torch.nn.Conv2d, Conv1D)
 
 def get_all_layers(model):
-    layers = []
-
-    for name, module in model.named_modules():
-        if isinstance(module, (torch.nn.Linear, torch.nn.Embedding, torch.nn.Conv2d, Conv1D)):
-            module_name = '.'.join(name.split('.')[4:]).split('.')[0]
-            if module_name.strip() != '':
-                layers.append(module_name)
-
-    return list(set(layers))
+    """Get all available model layer names."""
+    return _get_layer_names(model, True)
 
 
 def get_all_linear_layers(model):
+    """Get all available linear model layer names."""
+    return _get_layer_names(model)
+
+
+def _get_layer_names(model, is_all: bool = False):
     layers = []
 
     for name, module in model.named_modules():
-        if isinstance(module, torch.nn.Linear):
+        if isinstance(module, all_modules if is_all else torch.nn.Linear):
             module_name = '.'.join(name.split('.')[4:]).split('.')[0]
             if module_name.strip() != '':
                 layers.append(module_name)
