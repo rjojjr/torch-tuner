bfb407901c41f6eefba2cd8e9ea3aea694a7d3b8
Robert Kirchner <rjojjr91@live.com>
2024-08-30 15:03:43 -0500

    refactor/rename layers function argument to make more logical sense - rk

--- src/main/utils/model_utils.py
+++ src/main/utils/model_utils.py
@@ -6,19 +6,19 @@ all_modules = (torch.nn.Linear, torch.nn.Embedding, torch.nn.Conv2d, Conv1D)
 
 def get_all_layers(model):
     """Get all available model layer names."""
-    return _get_layer_names(model, True)
+    return _get_layer_names(model, False)
 
 
 def get_all_linear_layers(model):
     """Get all available linear model layer names."""
-    return _get_layer_names(model)
+    return _get_layer_names(model, True)
 
 
-def _get_layer_names(model, is_all: bool = False):
+def _get_layer_names(model, is_linear_only: bool = False):
     layers = []
 
     for name, module in model.named_modules():
-        if isinstance(module, all_modules if is_all else torch.nn.Linear):
+        if isinstance(module, all_modules if not is_linear_only else torch.nn.Linear):
             module_name = '.'.join(name.split('.')[4:]).split('.')[0]
             if module_name.strip() != '':
                 layers.append(module_name)
