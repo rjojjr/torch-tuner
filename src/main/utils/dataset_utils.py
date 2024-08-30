0a2fd1ae6b2bd6bde2419f3d2cbef1bf1c8c9192
Robert Kirchner <rjojjr91@live.com>
2024-08-30 14:59:56 -0500

    add description to dataset_utils function - rk

--- src/main/utils/dataset_utils.py
+++ src/main/utils/dataset_utils.py
@@ -5,6 +5,7 @@ from datasets import load_dataset as load_data_set, DatasetDict, Dataset, Iterab
 from arguments.arguments import TuneArguments
 
 def load_dataset(arguments: TuneArguments) -> Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]:
+    """Load dataset for SFT trainer."""
     if arguments.hf_training_dataset_id is not None:
         return load_data_set(arguments.hf_training_dataset_id, split='train')
     elif arguments.train_file.endswith(".jsonl"):
