5e93f77a1dbb18104a3661a903eff65dabed0650
Robert Kirchner <rjojjr91@live.com>
2024-08-30 13:45:08 -0500

    add return type to data-set loder function signature - rk

--- src/main/utils/training_utils.py
+++ src/main/utils/training_utils.py
@@ -1,8 +1,10 @@
-from datasets import load_dataset as load_data_set
+from typing import Union
+
+from datasets import load_dataset as load_data_set, DatasetDict, Dataset, IterableDatasetDict, IterableDataset
 
 from arguments.arguments import TuneArguments
 
-def load_dataset(arguments: TuneArguments):
+def load_dataset(arguments: TuneArguments) -> Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]:
     if arguments.hf_training_dataset_id is not None:
         return load_data_set(arguments.hf_training_dataset_id, split='train')
     elif arguments.train_file.endswith(".jsonl"):
