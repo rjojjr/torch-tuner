c8bd3da661da6a3f155ac8d6fc9dc5ab32f2f63a
Robert Kirchner <rjojjr91@live.com>
2024-08-30 15:24:13 -0500

    add 'generic' LLM type for tuner - rk

---
+++ src/main/modules/generic.py
@@ -0,0 +1,69 @@
+from utils.torch_utils import get_bnb_config_and_dtype
+
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from arguments.arguments import TuneArguments, MergeArguments, PushArguments
+import base.llm_base_module as base_module
+
+
+def merge(arguments: MergeArguments) -> None:
+    """Generic LLM type specific merge function."""
+    lora_dir = f"{arguments.output_dir}/adapters/{arguments.new_model}"
+    bnb_config, dtype = get_bnb_config_and_dtype(arguments)
+
+    base_model = AutoModelForCausalLM.from_pretrained(
+        arguments.base_model,
+        low_cpu_mem_usage=False,
+        return_dict=True,
+        torch_dtype=dtype
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained(lora_dir)
+    if arguments.padding_side is not None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = arguments.padding_side
+
+    base_module.merge_base(arguments, tokenizer, base_model, bnb_config)
+
+
+def push(arguments: PushArguments) -> None:
+    """Generic LLM type specific push function."""
+
+    bnb_config, dtype = get_bnb_config_and_dtype(arguments)
+
+    if not arguments.use_8bit and not arguments.use_4bit:
+        model = AutoModelForCausalLM.from_pretrained(
+            arguments.model_dir,
+            low_cpu_mem_usage=False,
+            return_dict=True,
+            torch_dtype=dtype
+        )
+    else:
+        model = AutoModelForCausalLM.from_pretrained(
+            arguments.model_dir,
+            low_cpu_mem_usage=True,
+            return_dict=True,
+            quantization_config=bnb_config,
+            device_map="auto"
+        )
+
+    tokenizer = AutoTokenizer.from_pretrained(arguments.model_dir)
+    if arguments.padding_side is not None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = arguments.padding_side
+
+    base_module.push_base(arguments, tokenizer, model)
+
+
+def fine_tune(arguments: TuneArguments) -> None:
+    """Generic LLM type specific fine-tune function."""
+    tokenizer = AutoTokenizer.from_pretrained(arguments.base_model)
+    if arguments.padding_side is not None:
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = arguments.padding_side
+
+    bnb_config, dtype = get_bnb_config_and_dtype(arguments)
+
+    model = AutoModelForCausalLM.from_pretrained(arguments.base_model, quantization_config=bnb_config, device_map="auto")
+
+    base_module.fine_tune_base(arguments, tokenizer, model)
--- src/main/utils/tuner_utils.py
+++ src/main/utils/tuner_utils.py
@@ -1,5 +1,6 @@
 # This is how I imagine modules for different LLM types should be imported and used to construct a 'Tuner' instance.
 import modules.llama as llama
+import modules.generic as generic_llm
 from base.tuner import Tuner, LLM_TYPES
 from exception.exceptions import ArgumentValidationException
 from typing import Callable
@@ -23,9 +24,10 @@ def parse_temp(temp: float) -> float:
 def _construct_tuner(prog_args) -> Tuner:
     _evaluate_supported_llm_type(prog_args.llm_type)
     match prog_args.llm_type:
-        # Since Llama is the only type currently
-        case _:
+        case 'LLAMA':
             tuner = Tuner(llama.fine_tune, llama.merge, llama.push, LLM_TYPES['llama'])
+        case _:
+            tuner = Tuner(generic_llm.fine_tune, generic_llm.merge, generic_llm.push, LLM_TYPES['generic'])
 
     return tuner
 
