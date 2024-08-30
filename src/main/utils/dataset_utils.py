774b4c3dfe0c9fd6944a4c95f632581df8e9518b
Robert Kirchner <rjojjr91@live.com>
2024-08-30 14:57:19 -0500

    handle training_data_dir tuning argument that ends in path seperator - rk

--- src/main/utils/dataset_utils.py
+++ src/main/utils/dataset_utils.py
@@ -8,6 +8,7 @@ def load_dataset(arguments: TuneArguments) -> Union[DatasetDict, Dataset, Iterab
     if arguments.hf_training_dataset_id is not None:
         return load_data_set(arguments.hf_training_dataset_id, split='train')
     elif arguments.train_file.endswith(".jsonl"):
-        return load_data_set("json", data_files={"train": f"{arguments.training_data_dir}/{arguments.train_file}"})
+        seperator = "/" if not arguments.training_data_dir.endswith("/") else ""
+        return load_data_set("json", data_files={"train": f"{arguments.training_data_dir}{seperator}{arguments.train_file}"})
     else:
         return load_data_set(arguments.training_data_dir, data_files={"train": arguments.train_file})
