c406ffe05e4a08c4f4f1addb4448da6caed4e10b
Robert Kirchner <rjojjr91@live.com>
2024-09-07 13:55:30 -0500

    update windows installer - rk

--- scripts/win/install-torch-tuner.bat
+++ scripts/win/install-torch-tuner.bat
@@ -1,22 +1,33 @@
 echo on
 echo "Installing Torch Tuner CLI"
 echo ""
-CD "%UserProfile%\.local"
 
-
+echo "Preparing installation directory"
+echo ""
 if exist "%UserProfile%\.local\torch-tuner" @RD /S /Q "%UserProfile%\.local\torch-tuner"
-
+CD "%UserProfile%\.local"
+echo "Getting latest CLI from github"
+echo ""
 git clone https://github.com/rjojjr/torch-tuner.git
 
 cd torch-tuner
 
 git checkout create-windows-os-installer-script
-
+echo ""
+echo "Installing python dependencies"
+echo ""
 python -m venv .\.venv && .\.venv\Scripts\activate.bat
 pip install -I -r requirements.txt
 deactivate
 
+echo ""
+echo "Finalizing install"
+echo ""
+
 xcopy scripts\win\torch-tuner C:\Windows\System32\torch-tuner
+
+icacls "%UserProfile%\.local\torch-tuner" /grant Users:F
+attrib -s C:\Windows\System32\torch-tuner
 
 echo ""
 echo "Torch Tuner CLI installed successfully!"
