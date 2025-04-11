e79add5fbd263bb66e5bcd58f6b25de2eb4a3427
Robert Kirchner <rjojjr91@live.com>
2025-04-11 16:32:19 -0400

    refactor hf util - rk

--- src/main/hf/util.py
+++ src/main/hf/util.py
@@ -10,6 +10,7 @@ def get_hf_username():
 
 
 def create_repo(repo_id, repo_type, is_private = True):
+    """Creates given HF repository if it does not exist."""
     api = HfApi()
     if not api.repo_exists(repo_id):
         print(f'creating HF repo {repo_id}')
@@ -21,9 +22,23 @@ def create_repo(repo_id, repo_type, is_private = True):
         print()
 
 
+def upload_folder(repo_id, folder_path, commit_message = None, path_in_repo = None, is_private = True, repo_type = 'model'):
+    api = HfApi()
+    create_repo(repo_id, repo_type, is_private)
+    api.upload_folder(
+        repo_id=repo_id,
+        folder_path=folder_path,
+        ignore_patterns=None,
+        allow_patterns=None,
+        repo_type=repo_type,
+        path_in_repo=path_in_repo,
+        commit_message=commit_message
+    )
+
+
 def upload_large_folder(repo_id, folder_path, is_private = True, repo_type = 'model'):
     api = HfApi()
-    create_repo(repo_id, 'model', is_private)
+    create_repo(repo_id, repo_type, is_private)
     api.upload_large_folder(
         repo_id=repo_id,
         folder_path=folder_path,
